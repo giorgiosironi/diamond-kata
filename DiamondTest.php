68fe2e3e7c8d5d5cb9f250d96c1fbc3cd649fb38
Giorgio Sironi <info@giorgiosironi.com>
2014-12-06 17:47:47 +0100

    Simplifying the problem for the first interesting test case: producing just a single line of the diamond

---
+++ DiamondTest.php
@@ -0,0 +1,41 @@
+<?php
+
+class DiamondTest extends \PHPUnit_Framework_TestCase
+{
+    public function testASingleLetter()
+    {
+        $this->assertEquals("A\n", (new Diamond("A"))->__toString());
+    }
+
+    public function testTwoLetters()
+    {
+        $this->assertEquals(
+              " A \n" 
+    //      . "B B\n" .
+    //      . " A \n"
+            , (new Diamond("B"))->__toString());
+    }
+}
+
+class Diamond
+{
+    private $finalLetter;
+    private $order;
+    private $letters = [
+        'A',
+        'B',
+    ];
+    
+    public function __construct($finalLetter)
+    {
+        $this->finalLetter = $finalLetter;
+        $this->order = array_search($finalLetter, $this->letters);
+    }
+
+    public function __toString()
+    {
+        $spaces = str_repeat(' ', $this->order);
+        $line = $spaces . 'A' . $spaces;
+        return $line . "\n";
+    }
+}
